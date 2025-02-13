bfbc0bdcb89d8abdb6bbcde81bcb8f65a329dfa1
Harsh <rajh87897@gmail.com>
2025-02-13 22:57:15 +0530

    Added a few (13) missing dog breeds

---
+++ src/main/resources/db/migration/V1.9__adding_breeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO `breed` VALUES (80,'2022-10-28 15:05:01','Australian Cattle','DOG'),(81,'2022-10-28 15:05:01','Miniature Pinscher','DOG'),(82,'2022-10-28 15:05:01','Goldendoodle','DOG'),
+(83,'2022-10-28 15:05:01','Schnoodle','DOG'),(84,'2022-10-28 15:05:01','Akita','DOG'),
+(85,'2022-10-28 15:05:01','Alaskan Malamute','DOG'),
+(86,'2022-10-28 15:05:01','Bernese Mountain Dog','DOG'),
+(87,'2022-10-28 15:05:01','Basset Hound','DOG'),
+(88,'2022-10-28 15:05:01','Belgian Malinois','DOG'),
+(89,'2022-10-28 15:05:01','Chow Chow','DOG'),
+(90,'2022-10-28 15:05:01','Irish Setter','DOG'),
+(91,'2022-10-28 15:05:01','Rhodesian Ridgeback','DOG'),
+(92,'2022-10-28 15:05:01','Samoyed','DOG'),
+(93,'2022-10-28 15:05:01','Tibetan Mastiff','DOG');
